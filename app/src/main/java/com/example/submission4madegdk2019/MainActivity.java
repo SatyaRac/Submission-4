2f118a9dbc47cd2a830029dd921e012896227e27
SatyaRac <satya.racerare87@gmail.com>
2019-09-05 17:16:53 +0700

    Ui

--- app/src/main/java/com/example/submission4madegdk2019/MainActivity.java
+++ app/src/main/java/com/example/submission4madegdk2019/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.submission4madegdk2019;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.submission4madegdk2019.fragment.FavoriteFragment;
+import com.example.submission4madegdk2019.fragment.MovieFragment;
+import com.example.submission4madegdk2019.fragment.TvFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new MovieFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.bottom_navigation, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.movies:
+                fragment = new MovieFragment();
+                break;
+
+            case R.id.tvshow:
+                fragment = new TvFragment();
+                break;
+
+            case R.id.favorite:
+                fragment = new FavoriteFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
